bebaf2b6824e34e7c159d70f476c6628aa5bd427
ENRIQUE MARTINEZ <enriquemartinezenriquem@outlook.com>
2023-02-28 17:46:20 -0500

    Agregar archivos de proyecto.

---
+++ TareasMVC/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
+
+namespace TareasMVC
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+    }
+}
---
+++ TareasMVC/Migrations/20230224202403_AdminRol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+IF NOT EXISTS(SELECT id FROM AspNetRoles WHERE id='26039c04-4dd1-43bf-88e3-946ab4b79ae7')
+BEGIN
+	INSERT AspNetRoles(id, [name], normalizedname)
+	VALUES('26039c04-4dd1-43bf-88e3-946ab4b79ae7','admin','ADMIN')
+END
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE AspNetRoles WHERE Id = '26039c04-4dd1-43bf-88e3-946ab4b79ae7'");
+        }
+    }
+}
